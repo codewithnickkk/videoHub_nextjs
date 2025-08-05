aa67a967710904b02a6be53e2d8a5e229dc113b9
Nikunj wadke <codewithnickk@gmail.com>
2025-08-05 15:45:55 +0530

    changes

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -3,14 +3,14 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import UploadVideo from "../components/uploadvideo";
-import { useRouter } from "next/router";
+
 
 export default async function UploadPage() {
   const session = await getServerSession(authOptions);
-  const Route = useRouter(); 
+  
   if (!session) {
-    // redirect("/api/auth/signin?callbackUrl=/upload");
-    Route.push("/login");
+    redirect("/login");
+   
   }
 
   return <UploadVideo />;
