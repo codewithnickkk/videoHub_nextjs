bf63e573e62a966f11f6c4606e2fc73fbd8774ef
Nikunj wadke <codewithnickk@gmail.com>
2025-08-05 16:02:51 +0530

    Fix: Removed useRouter from upload page to fix hydration issue

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,13 @@
-// No "use client"
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import UploadVideo from "../components/uploadvideo";
 
-
 export default async function UploadPage() {
   const session = await getServerSession(authOptions);
-  
+
   if (!session) {
     redirect("/login");
-  //  dhello
   }
 
   return <UploadVideo />;
